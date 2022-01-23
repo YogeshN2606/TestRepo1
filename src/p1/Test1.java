9eaea50ae02f2cdec94e9049a84caa7b8a3f4d60
Yogesh Nagar <yogesh61189@gmail.com>
2022-01-23 09:08:06 +0530

    added changes for B2

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -2,6 +2,7 @@ package p1;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("In P1 Test1");
+		System.out.println("In P1 Test1//////Addding");
+		System.out.println("Line 7 P1 Test1 B2 Branch");
 	}
 }
